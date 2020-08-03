cd25081304c02cb015b4ea775415246175fd9292
ajzajac <ajzajac@icloud.com>
2020-08-03 13:22:09 -0400

    started beverage page

--- src/components/Beverages.js
+++ src/components/Beverages.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Beverages() {
     return (
-        <div>
-            
+        <div className='beveragePage'>
+            <h2>Our mission for flavor is ongoing</h2>
+            <div className='beverageStory'>
+                <img src='https://static.seekingalpha.com/uploads/2019/2/8/48594911-1549660323811004_origin.png'></img>
+                <p>We have a vision of combining the freshest in-season fruits and herbs to make the most creative and flavorful combinations possible!</p>
+                <p>Our beverages are created by industry professionals as well as the community in almost an open source collaboration of flavors</p>
+            </div>
         </div>
     )
 }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import About from '../components/About'
+import Beverages from '../components/Beverages'
 
 export default class MainContainer extends Component {
     render() {
         return (
             <div>
+                <Beverages />
                 {/* <About /> */}
             </div>
         )

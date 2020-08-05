1a12807a0a789d111ec8f261e39c1f3e1894e481
ajzajac <ajzajac@icloud.com>
2020-08-05 18:10:40 -0400

    added account link to navbar ternary

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,9 +8,9 @@ export default function NavBar(props) {
                     <li><a href='/'>Brooklyn Bev</a></li>
                     <li><a href='/beverages'>Beverages</a></li>
                     <li><a href='/shop'>Shop</a></li>
-                    <li><a href='/blog'>Blog</a></li>
-                    <li><a href='/contact'>Contact</a></li>
-                    <li>{!props.user? <a href='/login'>Login</a> : <a href='/login' onClick={props.logOut}>Logout</a>}</li>
+                    {/* <li><a href='/blog'>Blog</a></li> */}
+                    <li><a href='/createbeverage'>Create Your Own</a></li>
+                    {!props.user? <li> <a href='/login'>Login</a> </li> : <><li><a href='/profile'>My Account</a></li>  <li> <a href='/login' onClick={props.logOut}>Logout</a></li></>}
                 </ul>
         </div>
     )

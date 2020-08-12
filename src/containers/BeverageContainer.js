196f8bb2977020a412e9d86bc917e0434a2d8527
ajzajac <ajzajac@icloud.com>
2020-08-12 16:07:26 -0400

    added carousel to bev page

--- src/containers/BeverageContainer.js
+++ src/containers/BeverageContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Beverage from '../components/Beverage'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 export default class BeverageContainer extends Component {
 
@@ -15,6 +17,23 @@ export default class BeverageContainer extends Component {
         return (
             <div className='beveragesPage'>
                 <h2>All Custom Beverages</h2>
+                <div className='bevCarousel'>
+                    <h3>Featured Beverages</h3>
+                    <Carousel autoPlay infiniteLoop className='beverageCarousel'>
+                        <div>
+                            <img src="https://www.acouplecooks.com/wp-content/uploads/2019/09/Whiskey-Sour-111s.jpg" />
+                            <p className="legend"></p>
+                        </div>
+                        <div>
+                            <img src="https://stevethebartender.com.au/wp-content/uploads/2015/11/winter-sun-cocktail.jpg" />
+                            <p className="legend"></p>
+                        </div>
+                        <div>
+                            <img src="https://149366112.v2.pressablecdn.com/wp-content/uploads/2019/01/winter-wonderland-cocktail.jpg" />
+                            <p className="legend"></p>
+                        </div>
+                    </Carousel>
+            </div>
                     <div className='beveragesList'>
                         {this.renderBeverages()}
                     </div> 

ae3808fe2c5c6f787992718fcc0e41d0b2ba40e9
ajzajac <ajzajac@icloud.com>
2020-08-13 13:48:54 -0400

    removed link from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,6 @@ export default function NavBar(props) {
                     <li><a href='/'>Brooklyn Bev</a></li>
                     <li><a href='/beverages'>Beverages</a></li>
                     <li><a href='/shop'>Shop</a></li>
-                    {/* <li><a href='/blog'>Blog</a></li> */}
                     <li><a href='/createyourown'>Create Your Own</a></li>
                     {!props.user? <li> <a href='/login'>Login</a> </li> : <><li><a href='/profile'>My Account</a></li>  <li> <a href='/login' onClick={props.logOut}>Logout</a></li></>}
                 </ul>

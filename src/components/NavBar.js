0405ee933c33572d60c0ab41d587491f634b1043
ajzajac <ajzajac@icloud.com>
2020-08-28 00:05:39 -0400

    h2 and h3 text styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar(props) {
     return (
         <div className='navBar'>
                 <ul>
                     <li><a href='/'><b>Brooklyn Bev</b></a></li>
-                    <li><a href='/beverages'>Beverages</a></li>
+                    <li><a href='/beverages'> Beverages</a></li>
                     {/* <li><a href='/shop'>Shop</a></li> */}
                     <li><a href='/createyourown'>Create Your Own</a></li>
                     {!props.user? <li> <a href='/login'>Login</a> </li> : <><li><a href='/profile'>My Account</a></li>  <li> <a href='/login' onClick={props.logOut}>Logout</a></li></>}
+                    <FontAwesomeIcon icon={faShoppingCart}/>
                 </ul>
         </div>
     )
